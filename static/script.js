cfcb85d0ce707b228e4994f0e71b9a830a058ef2
Niyaaz <niyaazdaniels1@gmail.com>
2024-08-01 09:25:51 +0200

    css changes

--- static/script.js
+++ static/script.js
@@ -1,9 +1,19 @@
 function showLoading() {
     const loadingMessage = document.getElementById("loadingMessage");
+    const weatherData = document.getElementById("weatherData");
+
+    // Clear the current weather data
+    weatherData.style.display = "none";
+
+    // Show the loading message
     loadingMessage.style.display = "block";
+
+    // Delay for loading spinner (optional)
     setTimeout(() => {
         loadingMessage.style.display = "none";
-    }, 3000); 
+    }, 2000); // You can adjust this time if needed
+
+    return true;
 }
 
 window.onload = function () {
@@ -11,6 +21,6 @@ window.onload = function () {
     elementsToFadeIn.forEach((element, index) => {
         setTimeout(() => {
             element.classList.add('fade-in');
-        }, index * 300); 
+        }, index * 300); // Adjust delay time between elements here (in milliseconds)
     });
 };
